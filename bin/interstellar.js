77acf0786a2415eb7c5ee900f20d42ad996a9d58
Bartek Nowotarski <bartek@nowotarski.info>
2019-03-25 15:29:50 +0100

    yarn

--- bin/interstellar.js
+++ bin/interstellar.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var spawn = require('child_process').spawn;
-var child = spawn(__dirname+'/../node_modules/gulp/bin/gulp.js', [
+var child = spawn(process.cwd()+'/node_modules/gulp/bin/gulp.js', [
   '--cwd', '.',
   '--gulpfile', __dirname+'/../gulpfile.js'
 ].concat(process.argv.slice(2)), {stdio: 'inherit'});
